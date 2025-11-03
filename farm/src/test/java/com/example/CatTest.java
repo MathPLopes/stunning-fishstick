b683137da0ea1f9c61e4f6ee01e51c8e89e71cfc
Matheus Patricio <163342842+MathPLopes@users.noreply.github.com>
2025-11-03 20:07:06 +0000

    Mudança de versão do junit + primeiro teste com classe Cat

---
+++ farm/src/test/java/com/example/CatTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+    
+    @Test
+    void testTalk(){
+        Cat cat = new Cat("Bigode");
+        String actual = cat.talk();
+        assertEquals("Bigode Meow!", actual);
+    }
+}
